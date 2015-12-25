879d8c05c72d1f595f948c46857c136f21dc32e6
hetengfei <hetengfei163@126.com>
2015-12-25 10:50:49 +0800

    init

---
+++ EmotionalKeyBoard/EmotionalKeyBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmotionalKeyBoard
+//
+//  Created by 赫腾飞 on 15/12/25.
+//  Copyright © 2015年 hetefe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
