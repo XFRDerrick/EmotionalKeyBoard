ee211bdc57efdf01b84c14ec8b0d9b30264a25f3
hetengfei <hetengfei163@126.com>
2015-12-26 00:42:38 +0800

    emoji表情项目创建

---
+++ EmojiFace/EmojiFaceTests/EmojiFaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiFaceTests.swift
+//  EmojiFaceTests
+//
+//  Created by 赫腾飞 on 15/12/26.
+//  Copyright © 2015年 hetefe. All rights reserved.
+//
+
+import XCTest
+@testable import EmojiFace
+
+class EmojiFaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
